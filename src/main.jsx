931194c22cefa54f96eb57252f6a03e2bb1e27d7
Charles <charlesngatia.cn@gmail.com>
2026-06-18 19:48:08 +0300

    fix: resolve merge conflicts

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
-import "./styles/global.css";
+import "./styles/globals.css";
 
 import { FavoritesProvider } from "./context/FavoritesContext";
 
