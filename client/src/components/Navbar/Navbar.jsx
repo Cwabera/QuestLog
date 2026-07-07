3eabee3dcb30f7241019c8e99799cdffa2709b6c
stevekahanya <wakahusteve@gmail.com>
2026-07-07 18:34:52 +0300

    feat: wire React auth and protected resource flows

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,18 +1,45 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink, useNavigate } from "react-router-dom";
+
+import { useAuth } from "../../context/useAuth";
 import "./Navbar.css";
 
 function Navbar() {
+  const { isAuthenticated, logout, user } = useAuth();
+  const navigate = useNavigate();
+
+  function handleLogout() {
+    logout();
+    navigate("/");
+  }
+
   return (
     <nav className="navbar">
       <div className="navbar-logo">
-        <Link to="/">🎮 QuestLog</Link>
+        <Link to="/">QuestLog</Link>
       </div>
 
       <div className="navbar-links">
-        <Link to="/">Home</Link>
-        <Link to="/games">Browse Games</Link>
-        <Link to="/favorites">Favorites</Link>
-        <Link to="/about">About</Link>
+        <NavLink to="/">Browse</NavLink>
+        {isAuthenticated && <NavLink to="/favorites">Favorites</NavLink>}
+        <NavLink to="/about">About</NavLink>
+      </div>
+
+      <div className="navbar-auth">
+        {isAuthenticated ? (
+          <>
+            <span>{user?.username}</span>
+            <button type="button" onClick={handleLogout}>
+              Log out
+            </button>
+          </>
+        ) : (
+          <>
+            <NavLink to="/login">Log in</NavLink>
+            <NavLink to="/register" className="navbar-auth__primary">
+              Register
+            </NavLink>
+          </>
+        )}
       </div>
     </nav>
   );
