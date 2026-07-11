fbaacf970029d62dd316b59768845f01c3929e48
Charles Wabera <charlesngatia.cn@gmail.com>
2026-07-11 15:52:14 +0300

    feat: complete backend authentication and JWT integration

--- server/run.py
+++ server/run.py
@@ -2,6 +2,8 @@ from app import create_app
 
 app = create_app()
 
+print("SECRET_KEY =", app.config["SECRET_KEY"])
+print("JWT_SECRET_KEY =", app.config["JWT_SECRET_KEY"])
+
 if __name__ == "__main__":
     app.run(debug=True)
-
