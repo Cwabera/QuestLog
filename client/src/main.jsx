354c7a3a4d59a6b446b37df43ed1787f5f85712a
Cwabera <charlesngatia.cn@gmail.com>
2026-07-13 21:41:32 +0300

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,3 +1,5 @@
+/.../src/services $ cd ~/Projects/QuestLog/client/src
+cat main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
@@ -5,16 +7,16 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./styles/globals.css";
 
-import { FavouritesProvider } from "./context/FavouritesContext";
 import { AuthProvider } from "./context/AuthContext";
+import { FavoritesProvider } from "./context/FavouritesContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <FavouritesProvider>
+        <FavoritesProvider>
           <App />
-        </FavouritesProvider>
+        </FavoritesProvider>
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
