3223d9d3fdde7a276dd4737f331a103bdfbaf18b
Maryann Kamau <5806maryannmuthoni@gmail.com>
2026-07-15 10:43:54 +0300

    bug fixes

--- client/src/pages/Profile/AdminDashboard.jsx
+++ client/src/pages/Profile/AdminDashboard.jsx
@@ -11,8 +11,7 @@ export default function AdminDashboard() {
         // Pull your live admin JWT authorization token out of your browser memory
         const token = localStorage.getItem("questlog_token") || localStorage.getItem("token");
         
-        const baseUrl = import.meta.env.VITE_API_BASE_URL ;
-        const response = await fetch(`${baseUrl}/users`, {
+        const response = await fetch("https://questlog-backend-7tvc.onrender.com/api/users", {
           method: "GET",
           headers: {
             "Content-Type": "application/json",
